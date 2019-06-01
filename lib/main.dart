677c22f58a454289b446cb2f84b604c7040f5713
Jason Wolf <jessy.yameogo@gmail.com>
2019-06-01 18:17:31 -0400

    ui for testing save and read functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  var data = await readData();
+
+  if (data != null) {
+    String message = await readData();
+    print(message);
+  }
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -23,38 +31,90 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _enterDataField = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Read/Write'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(13.4),
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: TextField(
+                  controller: _enterDataField,
+                  decoration: InputDecoration(labelText: 'Write Something'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+              ),
+              FlatButton(
+                onPressed: () {
+                  //save to file
+                  writeData(_enterDataField.text);
+                },
+                child: Column(
+                  children: <Widget>[
+                    Text('Save Data'),
+                    Padding(
+                      padding: EdgeInsets.all(14.5),
+                    ),
+                    FutureBuilder(
+                      future: readData(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<String> content) {
+                        if (content.hasData) {
+                          return Text(
+                            content.data.toString(),
+                            style: TextStyle(color: Colors.blueAccent),
+                          );
+                        } else {
+                          return Text('No data Saved!');
+                        }
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
   }
+}
 
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  return directory.path;
+}
 
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    String filename = 'data.txt';
-    return new File('$path/$filename');
-  }
+Future<File> get _localFile async {
+  final path = await _localPath;
+  String filename = 'data.txt';
+  return new File('$path/$filename');
+}
 
-  Future<File> writeData(String message) async {
+Future<File> writeData(String message) async {
+  final file = await _localFile;
+
+  //write to file
+  return file.writeAsString(message);
+}
+
+Future<String> readData() async {
+  try {
     final file = await _localFile;
 
-    //write to file
-    return file.writeAsString(message);
-  }
-
-  Future<String> readData() async {
-    try {
-      final file = await _localFile;
-
-      //read file content
-      String data = await file.readAsString();
-      return data;
-    } catch (e) {
-      return "Nothing Saved Yet!!";
-    }
+    //read file content
+    String data = await file.readAsString();
+    return data;
+  } catch (e) {
+    return "Nothing Saved Yet!!";
   }
 }
